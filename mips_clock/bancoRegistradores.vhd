7b17219f17e7d25a0bf93c6cf03e8c38f8573b05
Henry Rocha <henryrocha@protonmail.com>
2020-10-29 16:02:34 -0300

    BancoRegs format and add init values

--- mips_clock/bancoRegistradores.vhd
+++ mips_clock/bancoRegistradores.vhd
@@ -18,21 +18,57 @@ ENTITY bancoRegistradores IS
         enderecoC : IN STD_LOGIC_VECTOR((larguraEndBancoRegs - 1) DOWNTO 0);
         --
         dadoEscritaC : IN STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
-        escreveC : IN STD_LOGIC := '0';
+        escreveC     : IN STD_LOGIC := '0';
         --
-        saidaA   : OUT STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
-        saidaB   : OUT STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0)
+        saidaA : OUT STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
+        saidaB : OUT STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0)
     );
 END ENTITY;
 
 ARCHITECTURE comportamento OF bancoRegistradores IS
-
     SUBTYPE palavra_t IS STD_LOGIC_VECTOR((larguraDados - 1) DOWNTO 0);
     TYPE memoria_t IS ARRAY(2 ** larguraEndBancoRegs - 1 DOWNTO 0) OF palavra_t;
 
-    -- Declaracao dos registradores:
-    SHARED VARIABLE registrador : memoria_t;
+    FUNCTION initMemory
+        RETURN memoria_t IS VARIABLE tmp : memoria_t := (OTHERS => (OTHERS => '0'));
+    BEGIN
+        tmp(0)  := "00000000";
+        tmp(1)  := "00000000";
+        tmp(2)  := "00000000";
+        tmp(3)  := "00000000";
+        tmp(4)  := "00000000";
+        tmp(5)  := "00000000";
+        tmp(6)  := "00000000";
+        tmp(7)  := "00000000";
+        tmp(8)  := "00000001";
+        tmp(9)  := "00000001";
+        tmp(10) := "00000000";
+        tmp(11) := "00000000";
+        tmp(12) := "00000000";
+        tmp(13) := "00000000";
+        tmp(14) := "00000000";
+        tmp(15) := "00000000";
+        tmp(16) := "00000000";
+        tmp(17) := "00000000";
+        tmp(18) := "00000000";
+        tmp(19) := "00000000";
+        tmp(20) := "00000000";
+        tmp(21) := "00000000";
+        tmp(22) := "00000000";
+        tmp(23) := "00000000";
+        tmp(24) := "00000000";
+        tmp(25) := "00000000";
+        tmp(26) := "00000000";
+        tmp(27) := "00000000";
+        tmp(28) := "00000000";
+        tmp(29) := "00000000";
+        tmp(30) := "00000000";
+        tmp(31) := "00000000";
+        RETURN tmp;
+    END initMemory;
 
+    -- Declaracao dos registradores:
+    SHARED VARIABLE registrador : memoria_t := initMemory;
 BEGIN
     PROCESS (clk) IS
     BEGIN
